74370be8414609853ba1ddf55dcc1eafb143dfa8
TeoDor <retretik4@gmail.com>
2023-03-14 19:10:28 +0300

    SEAB-2 [feat] Додано рендеринг Ship на battleField

--- js/index.js
+++ js/index.js
@@ -9,3 +9,38 @@ const mainContainer = document.querySelector("#main");
 const battleFieldFactory = new BattleFieldFactory(mainContainer, config, elementBuilder);
 battleFieldFactory.render("my");
 battleFieldFactory.render("enemy");
+
+/**
+ * @param {number} startX
+ * @param {number} startY
+ * @param {number} numDeck
+ * @param {boolean} horizontal
+ */
+function renderShip(startX, startY, numDeck, horizontal) {
+    const elName = [];
+    if (horizontal === true) {
+        for (let i = startY; i < startY + numDeck; i++) {
+            const str = `#my .battle-field-cell[x^='${startX}'][y^='${i}']`;
+            elName.push(document.querySelector(str));
+        }
+    }
+    if (horizontal === false) {
+        for (let i = 0; i < numDeck; i++) {
+            const str = `#my .battle-field-cell[x^='${startX}'][y^='${startY}']`;
+            elName.push(document.querySelector(str));
+        }
+    }
+    elName.forEach(elem => {
+        elem.appendChild(elementBuilder
+            .createElement()
+            .setClassName("ship")
+            .build());
+    });
+}
+
+renderShip(0, 3, 4, true);
+renderShip(1, 1, 3, false);
+renderShip(7, 2, 3, false);
+renderShip(2, 9, 3, true);
+renderShip(7, 10, 2, false);
+renderShip(6, 6, 1, true);
